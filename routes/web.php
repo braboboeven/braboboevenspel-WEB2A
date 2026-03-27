a6c6e473af87b92738638c36fe7d5086f38bf6e5
Zakaria Aourzag <git@zakariao.nl>
2026-03-27 12:25:48 +0100

    Wire up game pages in router

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
     Route::livewire('spel', 'pages::spel')->name('spel');
     Route::livewire('docent', 'pages::docent')->name('docent');
+    Route::view('spelregels', 'pages.regels')->name('regels');
+    Route::view('wacht', 'pages.wacht')->name('wacht');
+    Route::livewire('leaderboard', 'pages::leaderboard')->name('leaderboard');
 });
 
 require __DIR__.'/settings.php';
